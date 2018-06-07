578b87f0a0ee2daa7b7de80d3473cc2d0a65ee14
Flowaria <wnsghgj1234@naver.com>
2018-06-08 00:24:28 +0900

    초기 커밋.

---
+++ Valve/VTF/VMT.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Valve.KeyValue;
+
+namespace Valve.TextureFormat
+{
+    public class VMT
+    {
+        public VMT()
+        {
+            KeyValues kv = KVFile.ImportKeyValue("filename", false);
+            var basetexture = (string)kv.Root.GetValue("$baseTexture");
+        }
+    }
+}
